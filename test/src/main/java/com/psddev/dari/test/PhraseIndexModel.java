307f2e335a0ae9eec993da52c1b0fa3f61b56221
billbell <billnbell@gmail.com>
2017-06-20 16:16:29 -0600

    To be like all other tests

--- test/src/main/java/com/psddev/dari/test/PhraseIndexModel.java
+++ test/src/main/java/com/psddev/dari/test/PhraseIndexModel.java
@@ -1,16 +1,8 @@
 package com.psddev.dari.test;
 
-import com.psddev.dari.db.Database;
 import com.psddev.dari.db.Record;
 
 class PhraseIndexModel extends Record {
 
-    public static PhraseIndexModel getInstance(Database db) {
-        PhraseIndexModel tr = new PhraseIndexModel();
-        tr.getState().setDatabase(db);
-        return tr;
-    }
-
     @Indexed String testString;
 }
-
